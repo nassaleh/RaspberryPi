06f4b3769621189b7e950565fc7e8a1d9db7f9ab
Nicolas Assaleh <nick.assaleh@gmail.com>
2024-01-03 18:21:10 -0500

    Added abolity to read from button

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ void part1()
     int gpio24 = 24;
 
     FILE *led = fopen("/sys/class/gpio/gpio24/value", "w");
+    FILE *button = fopen("/sys/class/gpio/gpio17/value", "r");
 
     if (led == NULL)
     {
@@ -22,12 +23,21 @@ void part1()
 
     while (true)
     {
-        fprintf(led, "%d", 1);
-        // fputs("1", led);
-        fflush(led);
-        sleep(1);
-        fprintf(led, "%d", 0);
-        fflush(led);
-        sleep(1);
+        char line[1];
+        fgets(line, sizeof(line), button);
+
+        if (line[0] == '1')
+        {
+            fprintf(led, "%d", 1);
+            // fputs("1", led);
+            fflush(led);
+            sleep(1);
+        }
+        else if (line[0] == '0')
+        {
+            fprintf(led, "%d", 0);
+            fflush(led);
+            sleep(1);
+        }
     }
 }
