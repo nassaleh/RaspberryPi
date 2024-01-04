94bf41d34217e983bd1f7a45b47a8f6416b9f5ed
Nicolas Assaleh <nick.assaleh@gmail.com>
2024-01-04 15:58:30 -0500

    Added notes, fixed main to read value from switch

--- README.md
+++ README.md
@@ -12,5 +12,8 @@ Use system calls in C instead of writing to sysfs directly
 
 use open/write instead of sopen
 
+## Use poll system call
 poll file for switch
-Use poll system call
+https://man7.org/linux/man-pages/man2/poll.2.html
+
+Use C library `poll` (look up man poll)
--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 void part1();
+void set_gpio_direction(int pin, const char *direction);
 
 int main()
 {
@@ -11,6 +17,10 @@ int main()
 
 void part1()
 {
+    // Init GPIO
+    set_gpio_direction(17, "in");
+    set_gpio_direction(24, "out");
+
     int gpio24 = 24;
 
     FILE *led = fopen("/sys/class/gpio/gpio24/value", "w");
@@ -23,21 +33,86 @@ void part1()
 
     while (true)
     {
-        char line[1];
-        fgets(line, sizeof(line), button);
+        char line[4];
+        // fgets(line, sizeof(line), button);
+        size_t bytesRead = fread(line, sizeof(char), sizeof(line), button);
+        fseek(button, 0, 0);
+        
 
         if (line[0] == '1')
         {
             fprintf(led, "%d", 1);
             // fputs("1", led);
             fflush(led);
-            sleep(1);
+            sleep(0.1);
         }
         else if (line[0] == '0')
         {
             fprintf(led, "%d", 0);
             fflush(led);
-            sleep(1);
+            sleep(0.1);
+        }
+
+        sleep(0.1);
+    }
+}
+
+void set_gpio_direction(int pin, const char *direction)
+{
+    char export_path[50];
+    char direction_path[50];
+
+    // Create the paths for export and direction files
+    snprintf(export_path, sizeof(export_path), "/sys/class/gpio/export");
+    snprintf(direction_path, sizeof(direction_path), "/sys/class/gpio/gpio%d/direction", pin);
+
+    // Open /sys/class/gpio/export for writing
+    int gpio_export_fd = open(export_path, O_WRONLY);
+    if (gpio_export_fd == -1)
+    {
+        perror("Error opening export file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Write GPIO pin number to export file
+    char pin_str[4];
+    snprintf(pin_str, sizeof(pin_str), "%d", pin);
+    if (write(gpio_export_fd, pin_str, strlen(pin_str)) == -1)
+    {
+        perror("Error writing to export file");
+        // close(gpio_export_fd);
+        // exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("GPIO pin %d exported successfully\n", pin);
+    }
+
+    // Close the export file
+    close(gpio_export_fd);
+
+    // Open direction file for writing
+    int gpio_direction_fd = open(direction_path, O_WRONLY);
+    if (gpio_direction_fd == -1)
+    {
+        perror("Error opening direction file");
+        // exit(EXIT_FAILURE);
+    }
+    else
+    {
+        // Write direction to the direction file
+        if (write(gpio_direction_fd, direction, strlen(direction)) == -1)
+        {
+            perror("Error writing to direction file");
+            close(gpio_direction_fd);
+            // exit(EXIT_FAILURE);
+        }
+        else
+        {
+            printf("GPIO pin %d set to %s direction successfully\n", pin, direction);
         }
     }
+
+    // Close the direction file
+    close(gpio_direction_fd);
 }
